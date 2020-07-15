ac0f2834046e8c0b9cf62ee64a7ecfa26b884f76
key5771 <ksj57715@hanmail.net>
2020-07-15 14:01:19 +0900

    Create Project Font 추가, image asset 추가

---
+++ WhatToEat/WhatToEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToEat
+//
+//  Created by 김기현 on 2020/07/15.
+//  Copyright © 2020 김기현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
